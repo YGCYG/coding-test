bc12656411d536557a1cd60b5532b980cf2e8e4a
geon.choi <geon.choi@sk.com>
2023-01-17 18:25:45 +0900

    230117

---
+++ src/main/kotlin/programmers/level0/day1/Division.kt
@@ -0,0 +1,5 @@
+package programmers.level0.day1
+
+class Division {
+   fun solution(num1: Int, num2: Int): Int = num1 / num2
+}
---
+++ src/main/kotlin/programmers/level0/day2/Division2.kt
@@ -0,0 +1,5 @@
+package programmers.level0.day2
+
+class Division2 {
+   fun solution(num1: Int, num2: Int): Int = ((num1.toDouble() / num2) * 1000).toInt()
+}
